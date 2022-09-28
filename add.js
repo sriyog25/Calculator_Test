26e7e8ac00a6cb3b0060cb48ee38f2a30f91c731
sriyog.dixit@itilite.in <sriyog.dixit@gmail.com>
2022-09-28 14:32:39 +0530

    Add fixed

--- add.js
+++ add.js
@@ -1,6 +1,6 @@
 function add(){
 var n1=document.getElementById("n1").value;
 var n2=document.getElementById("n2").value;
-var res=n1+n2;
+var res=parseInt(n1)+parseInt(n2);
 document.getElementById("res").value=res;
 }
